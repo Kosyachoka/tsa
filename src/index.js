4cff5a7decbf392cbf337ef767fc0ba874d11063
KonstantinZubanyuk <konstantinz@cpart.co.il>
2018-11-21 18:08:38 +0200

    New response format

--- src/index.js
+++ src/index.js
@@ -1,31 +1,50 @@
 const PORT = process.env.PORT || 5000;
+const TorrentSearchApi = require('torrent-search-api');
+TorrentSearchApi.loadProvider(ThePirateBayProvider());
+//TorrentSearchApi.enablePublicProviders();
+TorrentSearchApi.enableProvider('Torrent9');
+TorrentSearchApi.enableProvider('1337x');
+TorrentSearchApi.enableProvider('Torrentz2');
+TorrentSearchApi.enableProvider('ThePirateBayProxy');
 
-var TorrentSearchApi = require('torrent-search-api');
-TorrentSearchApi.enablePublicProviders();
+console.log("Using providers:");
+console.log(TorrentSearchApi.getActiveProviders().map(t => t.name));
 
 var express = require("express");
 var app = express();
 
-app.get('/', requestHandler);
-app.get('/search/:query/:category', requestHandler);
+app.use(express.json());
+
+app.get('/', (request, response, next) => {
+  response.statusCode = 404;
+  response.end();
+});
+
+app.post('/', (request, response, next) => {
+
+  let searchRequest = request.body;
+
+  if(checkRequest(searchRequest)){
+    let searchPromise = (searchRequest.torrent === undefined || searchRequest.torrent == ''
+    ? TorrentSearchApi.search(searchRequest.query, searchRequest.category, 10) 
+    : TorrentSearchApi.search([searchRequest.torrent], searchRequest.query, searchRequest.category, 10));
+
+    searchPromise.then((torrents) => {
+      response.json(torrents);
+      response.end();
+    }).catch((e) => {
+      console.log(e);
+      response.statusCode = 500;
+      response.end();
+    });
+  }
+  else{
+    response.statusCode = 404;
+    response.end();
+  }
+});
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
-function requestHandler(req, res, next){
-  if(checkRequest(req.params)){
-    TorrentSearchApi.search(req.params.query, req.params.category, 20).then(torrents => {
-        res.json(torrents);
-        res.end();
-      }).catch(e => {
-        res.statusCode = 500;
-        res.end();
-      });
-    }
-    else{
-      res.statusCode = 404;
-      res.end();
-    }
-};
 
 function checkRequest(params){
   let result = (params.query !== undefined && 
@@ -37,5 +56,37 @@ function checkRequest(params){
   else{
     console.log('request check fail {params.query:"' + params.query +'", params.category:"' + params.category + '"}');
     return false;
+  }
+}
+
+function ThePirateBayProvider(){
+  return {
+    "name": "ThePirateBayProxy",
+    "baseUrl": "https://pirateproxy.gdn/",
+    "searchUrl": "/search/{query}/0/7/{cat}",
+    "categories": {
+       "All": "0",
+       "Audio": "100",
+       "Video": "200",
+       "Applications": "300",
+       "Games": "400",
+       "Porn": "500",
+       "Other": "600",
+       "Top100": "url:/top/all"
+    },
+    "defaultCategory": "All",
+    "resultsPerPageCount": 30,
+    "itemsSelector": "#searchResult tr",
+    "itemSelectors": {
+       "title": "a.detLink@text",
+       "time": "font.detDesc@text | match:\"Uploaded\\s(.+?),\"",
+       "seeds": "td:nth-child(3) | int",
+       "peers": "td:nth-child(4) | int",
+       "size": "font.detDesc@text | match:\"Size\\s(.+?),\"",
+       "magnet": "a[title=\"Download this torrent using magnet\"]@href",
+       "desc": "div.detName a@href"
+    },
+    "paginateSelector": "a:has(img[alt=\"Next\"])@href",
+    "torrentDetailsSelector": "div.nfo > pre@text"
   }
 }
