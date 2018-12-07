af0f5d7adb75a939598fb55118ef080340f23839
Kosyachoka <kosyachoka@gmail.com>
2018-12-07 09:00:02 +0200

    port chnaged

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 const TorrentSearchApi = require('torrent-search-api');
 TorrentSearchApi.loadProvider(ThePirateBayProvider());
 //TorrentSearchApi.enablePublicProviders();
